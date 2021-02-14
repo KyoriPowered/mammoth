45be4bf86f2653dcf8680f91f147237e227669e9
Riley Park <riley.park@meino.net>
2021-02-14 00:25:10 -0800

    move to mammoth package

--- src/main/java/net/kyori/mammoth/Extensions.java
+++ src/main/java/net/kyori/mammoth/Extensions.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package net.kyori.gradle.api;
+package net.kyori.mammoth;
 
 import org.gradle.api.plugins.ExtensionContainer;
 
