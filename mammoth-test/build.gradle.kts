7353da21a74672596dff2c5f61ca244dd27e25da
zml <zml@stellardrift.ca>
2022-01-05 19:17:52 -0800

    build: Move away from Kotlin buildscript

--- mammoth-test/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-description = "JUnit extensions for testing Gradle plugins"
-
-dependencies {
-  api(gradleApi())
-  api(gradleTestKit())
-  api("org.junit.jupiter:junit-jupiter-api:5.7.2")
-  implementation("org.junit.platform:junit-platform-commons:1.7.2")
-}
